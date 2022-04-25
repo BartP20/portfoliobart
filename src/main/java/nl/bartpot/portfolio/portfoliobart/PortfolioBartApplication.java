9360eed22cdd479b8962719fc643a50aaf9baca5
BartP20 <bart.pot@duo.nl>
2022-04-25 15:10:50 +0200

    Initial commit

---
+++ src/main/java/nl/bartpot/portfolio/portfoliobart/PortfolioBartApplication.java
@@ -0,0 +1,13 @@
+package nl.bartpot.portfolio.portfoliobart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioBartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/bartpot/portfolio/portfoliobart/PortfolioBartApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.bartpot.portfolio.portfoliobart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
